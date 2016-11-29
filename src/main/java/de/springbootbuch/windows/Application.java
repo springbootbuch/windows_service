ef74f383e7143fa4856742356ca9d4e633b56111
Michael J. Simons <michael@simons.ac>
2016-11-29 18:22:34 +0100

    Create windows_service

---
+++ src/main/java/de/springbootbuch/windows/Application.java
@@ -0,0 +1,25 @@
+package de.springbootbuch.windows;
+
+import javax.management.InstanceNotFoundException;
+import javax.management.remote.JMXConnector;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(final String... args) throws Exception {
+        if (args.length != 2 || !args[0].trim().equalsIgnoreCase("stopService")) {
+            SpringApplication.run(Application.class, args);
+        } else {
+            try (JMXConnector connector = AdminClient.connect(Integer.parseInt(args[1]))) {
+                try {
+                    new AdminClient(connector.getMBeanServerConnection(), AdminClient.DEFAULT_OBJECT_NAME).stop();
+                } catch (InstanceNotFoundException ex) {
+                    throw new IllegalStateException("Spring application lifecycle JMX bean not "
+                            + "found, could not stop application gracefully", ex);
+                }
+            }
+        }
+    }
+}
