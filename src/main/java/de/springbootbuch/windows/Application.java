715289d867f934d610b61d2be30c308dc20ae187
Michael J. Simons <michael@simons.ac>
2017-03-05 10:57:36 +0100

    Adapt formatting for examples

--- src/main/java/de/springbootbuch/windows/Application.java
+++ src/main/java/de/springbootbuch/windows/Application.java
@@ -1,5 +1,7 @@
 package de.springbootbuch.windows;
 
+import static de.springbootbuch.windows.AdminClient.connect;
+import static java.lang.Integer.parseInt;
 import javax.management.InstanceNotFoundException;
 import javax.management.remote.JMXConnector;
 import org.springframework.boot.SpringApplication;
@@ -8,16 +10,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-    public static void main(final String... args) throws Exception {
-        if (args.length != 2 || !args[0].trim().equalsIgnoreCase("stopService")) {
-            SpringApplication.run(Application.class, args);
-        } else {
-            try (JMXConnector connector = AdminClient.connect(Integer.parseInt(args[1]))) {
-                new AdminClient(connector.getMBeanServerConnection(), AdminClient.DEFAULT_OBJECT_NAME).stop();
-            } catch (InstanceNotFoundException ex) {
-                throw new IllegalStateException("Spring application lifecycle JMX bean not "
-                        + "found, could not stop application gracefully", ex);
-            }
-        }
-    }
+	public static void main(final String... args)
+		throws Exception {
+		if (args.length != 2 || 
+			!args[0].trim().equalsIgnoreCase("stopService")
+		) {
+			SpringApplication.run(
+				Application.class, args);
+		} else {
+			final int port = parseInt(args[1]);
+			try (JMXConnector connector = connect(port)) {
+				new AdminClient(
+					connector.getMBeanServerConnection(), 
+					AdminClient.DEFAULT_OBJECT_NAME)
+				.stop();
+			} catch (InstanceNotFoundException ex) {
+				throw new IllegalStateException(
+					"Spring application lifecycle "
+					+ " JMX bean not found,"
+					+ " could not stop application"
+					+ " gracefully",
+					ex
+				);
+			}
+		}
+	}
 }
